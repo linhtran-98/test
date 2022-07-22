1672047e68ef67bc4eab5101a8a14143039df37d
Linh <linhtran20021998@gmail.com>
2022-07-22 16:51:31 +0700

    Add project

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\HelloEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class EmailController extends Controller
+{
+    public function sendEmail()
+    {
+        $reveiverEmailAddress = "conchomuc1998@gmail.com";
+        Mail::to($reveiverEmailAddress)->send(new HelloEmail);
+
+        if (Mail::failures() != 0) {
+            return "Email has been sent successfully.";
+        }
+        return "Oops! There was some error sending the email.";
+    }
+}
